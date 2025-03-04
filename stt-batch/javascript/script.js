cddf74bdd8fb061527310d586c237eef33ee4473
souravmishra3093 <sourav.mishra@reverieinc.com>
2025-03-04 15:25:48 +0530

    STT-File Added

--- stt-batch/javascript/script.js
+++ stt-batch/javascript/script.js
@@ -1,8 +1,13 @@
-function transcribeAudio() {
+async function transcribeAudio() {
     const fileInput = document.getElementById("audioFile");
     const audioPlayer = document.getElementById("audioPlayer");
     const transcriptionOutput = document.getElementById("transcription");
-    const language = document.getElementById("language").value; 
+    const language = document.getElementById("language").value;
+
+    const reverieClient = new ReverieClient({
+        apiKey: "<YOUR-API-KEY>",
+        appId: "<YOUR-APP-ID>"
+    });
 
     if (fileInput.files.length === 0) {
         alert("Please select an audio file first.");
@@ -10,30 +15,19 @@ function transcribeAudio() {
     }
 
     const file = fileInput.files[0];
-    const formData = new FormData();
-    formData.append("audio_file", file);
 
     const objectURL = URL.createObjectURL(file);
     audioPlayer.src = objectURL;
 
-    fetch("https://revapi.reverieinc.com/", {
-        method: "POST",
-        headers: {
-            "src_lang": language,
-            "domain": "generic",
-            "REV-APPNAME": "stt_file",
-            "REV-API-KEY": "your_api_key",
-            "REV-APP-ID": "your_app_id",
-
-        },
-        body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-        transcriptionOutput.textContent = data.transcription || "Transcription failed.";
-    })
-    .catch(error => {
+    try {
+        const response = await reverieClient.stt_batch({
+            audioFile: file,
+            src_lang: language
+        });
+        console.log('hey', response);
+        transcriptionOutput.textContent = response;
+    } catch (error) {
         console.error("Error:", error);
         transcriptionOutput.textContent = "Error processing the request.";
-    });
+    }
 }
